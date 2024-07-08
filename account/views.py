2f12c56ea4b931fc466904495205d8f372aea90b
pooriya <adibradp@gmail.com>
2024-07-08 14:47:38 +0330

    the login and signin be designed

--- account/views.py
+++ account/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login,logout
 # Create your views here.
 def signin(request):
     if request.method == 'POST':
-        username = request.POST['username']
+        NationCode = request.POST['NationCode']
         password = request.POST['password']
+        pass
+def logout(request):
+    logout(request)
+    pass
+def login(request):
+    if request.method == 'POST':
+        NationCode = request.POST['NationCode']
+        password = request.POST['password']
+        user = authenticate(request, NationCode=NationCode, password=password)
+        if user is not None:
+            login(request, user)
+            pass
+
 
