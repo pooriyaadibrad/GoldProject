86bc5234b15e3d2a728180f7de51aeca278f66c4
pooriya <adibradp@gmail.com>
2024-07-10 16:31:46 +0330

    i set sign up and login

--- account/urls.py
+++ account/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('', views.Login, name='login'),
     path('LoginRequest/', views.LoginRequest, name='LoginRequest'),
     path('signin/', views.signin, name='index'),
+    path('admin/', views.admin, name='admin'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,22 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login,logout
+from django.contrib.auth.models import User
+from . import models
+from django.contrib import messages
 # Create your views here.
 def signin(request):
     if request.method == 'POST':
         NationCode = request.POST['NationCode']
         password = request.POST['password']
-        pass
+        user1=models.user(NationCode=NationCode,password=password)
+        user1.save()
+        user2=User.objects.create_user(username=NationCode,password=password)
+        user2.save()
+        login(request,user2)
+        return render(request,template_name='Admin.html')
+
+
+    else:
+        return render(request,template_name='SignUp.html')
 def Logout(request):
     logout(request)
-    pass
+    return redirect('login')
 def Login(request):
-        return render(request,template_name='Login.html',context={'NationCode':'NationCode','password':'password'})
+        return render(request,template_name='Login.html')
 
 
 def LoginRequest(request):
-    print('this is test')
     if request.method == 'POST':
         NationCode = request.POST['NationCode']
         password = request.POST['password']
-        return render(request, 'Login.html',{'NationCode':NationCode,'password':password})
+        user=authenticate(request,NationCode=NationCode,password=password)
+        if user is not None:
+            login(request,user)
+            message=messages.success(request,'با موفقیت وارد شدید!')
+            return redirect('admin')
+        else:
+            message=messages.success(request,'نام کاربری یا رمز عبور درست نمی باشد!')
+            return render(request,template_name='Login.html',context={'message':message})
 
+
+def admin(request):
+    return render(request,template_name='Admin.html')
