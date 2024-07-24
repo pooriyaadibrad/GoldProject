de5eb50e842e336100a600dda0b7cbdde85d8def
A.H.O <a.h.o@MacBook-Pro.local>
2024-07-24 18:23:30 +0330

    final-config

--- GoldenProject/settings.py
+++ GoldenProject/settings.py
@@ -125,15 +125,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT=os.path.join(BASE_DIR , 'staticfiles')
+STATIC_URL = '/public/static/'
+STATIC_ROOT=os.path.join(BASE_DIR, "public","static")
 STATICFILES_DIRS = [BASE_DIR /
                     'adminAPP/static',
                     'account/static',
                     'CustomerApp/static',
                     ]
-MEDIA_URL='media/'
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL='/public/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR, "public","media")
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
