78f1edbb456e165137261178a70347441a914ef6
pooriya adib rad <103428413+pooriyaadibrad@users.noreply.github.com>
2024-07-29 23:14:45 +0330

    update for set postgresql
    change sqlite to postgres

--- GoldenProject/settings.py
+++ GoldenProject/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'GoldenProject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -97,6 +97,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
